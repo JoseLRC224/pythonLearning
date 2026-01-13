91c9cca6f6765997a4b6ad9b5bdec9bb614fd198
JoseLRC224 <joselurc24@gmail.com>
2026-01-13 02:41:02 -0600

    Día 1: Solución al error de tipos y uso de f-strings

---
+++ leccionesBasicas/reto1.py
@@ -0,0 +1,12 @@
+mi_nombre = "José Luis"
+mi_edad = 24
+
+#maneras de imprimir
+print("---------- Manera 1 ----------")
+print("Hola, me llamo " + mi_nombre + " y tengo " + str(mi_edad) + " años")
+print(" ")
+print("---------- Manera 2 ----------")
+print("Hola, me llamo", mi_nombre, "y tengo", mi_edad, "años")
+print(" ")
+print("---------- Manera 3 ----------")
+print(f"Hola, me llamo {mi_nombre} y tengo {mi_edad} años")
